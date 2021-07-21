b5e4eedcc7456683f758de6f2e763d40d3b6d55a
Gideon Odiase <gideon.odiase@shuttlers.ng>
2021-07-22 00:54:15 +0100

    comppleted profile and listing section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter } from 'react-router-dom';
 import Routes from '../src/Routes';
+import './App.css';
 
 function App() {
   return (
---
+++ src/Components/Listing/index.jsx
@@ -0,0 +1,57 @@
+import React, { Fragment } from 'react'
+import styles from './listing.module.css'
+import {faLinkedin, faGithub} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const Listing = () => {
+  return (
+    <>
+      <div
+        className={styles.container}
+      >
+        <h2
+          className={styles.name_}
+        >
+          Gideon Odiase
+        </h2>
+        <h4
+          className={styles.role}
+        >
+          Software Engineer
+        </h4>
+        <div
+          className={styles.social}
+        >
+          <FontAwesomeIcon
+            size="2x"
+            style={{ color: 'white' }}
+            icon={faLinkedin} 
+            className={styles.iconSocial}
+          />
+          <FontAwesomeIcon
+            size="2x" 
+            style={{ color: 'white' }} 
+            icon={faGithub}
+            className={styles.iconSocial}
+          />
+        </div>
+        <div
+          className={styles.buttonDiv_}
+        >
+          <input
+            type="button"
+            className={styles.download}
+            value="Download CV"
+          />
+        </div>
+        <p
+          style={{color:'white', textAlign:"center"}}
+        >
+          © {new Date().getFullYear()} All rights reserved.
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default Listing;
